e20a96cff9a55254465b4291a3bffb5f830475cd
Andrew Nelson <hello@andrewnelson.net>
2025-09-25 13:41:33 -0700

    docs(frontend): remove old frontend section now that pages are relocated

--- pmis-docs/content/guides/frontend/_meta.js
+++
@@ -1,7 +0,0 @@
-export default {
-    "css-tailwind-isolation": "CSS & Tailwind Isolation",
-    "isolated-local-dev": "Isolated Local Dev Model",
-    "frontend-styling-strategy": "Frontend Styling Strategy"
-}
-
-
